a7b7e9fe3006ea1355afd83fc8959170d3b97c6f
Seth Thoburn <seth.thoburn@gmail.com>
2020-03-07 13:03:03 -0800

    Fix url setting

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import path, include
+from django.urls import path, include
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 
