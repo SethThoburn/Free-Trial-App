ab4e05ba47f8a0b43805e99436dfc601ad6e33d5
Seth Thoburn <seth.thoburn@gmail.com>
2020-03-09 16:32:05 -0700

    -

--- api/views.py
+++ api/views.py
@@ -5,6 +5,7 @@ from .models import Trial
 
 def email(request):
     if request.method == 'POST':
+        print(request.POST)
         subject = request.POST.get('subject', '')
         Trial.objects.create(
             name = subject,
